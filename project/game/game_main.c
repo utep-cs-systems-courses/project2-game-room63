66a4556214c72cf7fc1e2e5906ba68cb4e024119
student <zgomez4@miners.utep.edu>
2021-11-15 15:06:52 -0700

    fixed a bug

--- project/game/game_main.c
+++ project/game/game_main.c
@@ -9,7 +9,7 @@ int main (void)
   buzzer_init();
   led_init();
   switch_init();
-  enableWDTInterrupt(); /* enable periodic interrupt */
+  enableWDTInterrupts(); /* enable periodic interrupt */
   configureClocks();     /* setup master oscillator, CPU & peripheral clocks */
 
   or_sr(0x18);         /* CPU off, GIE on */
