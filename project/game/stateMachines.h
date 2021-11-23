965c31f4c204cb5d01568b50c4421c59e6c362c4
student <zgomez4@miners.utep.edu>
2021-11-22 21:30:12 -0700

    better description added.

--- project/game/stateMachines.h
+++ project/game/stateMachines.h
@@ -1,21 +1,24 @@
 #ifndef stateMachine_included
 #define stateMachine_included
+ // Red LED.
+ void dim25r();
+ void dim50r();
+ void dim75r();
+ void dim100r();
 
-void dim25r();
-void dim50r();
-void dim75r();
-void dim100r();
+ // Green LED.
+ void dim25g();
+ void dim50g();
+ void dim75g();
+ void dim100g();
 
-void dim25g();
-void dim50g();
-void dim75g();
-void dim100g();
+ // Combination of Green and Red LED.
+ void dim25gr();
+ void dim50gr();
+ void dim75gr();
+ void dim100gr();
 
-void dim25gr();
-void dim50gr();
-void dim75gr();
-void dim100gr();
-
-void sound();
+ // Secuence of sounds.
+ void sound();
 
 #endif // included
