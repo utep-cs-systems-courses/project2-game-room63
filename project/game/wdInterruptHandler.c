21c5f0e2da01cd0495705b833ed406952207e2b5
student <zgomez4@miners.utep.edu>
2021-11-23 16:13:04 -0700

    bug solved

--- project/game/wdInterruptHandler.c
+++ project/game/wdInterruptHandler.c
@@ -6,20 +6,44 @@
 
 
 // variables to indicate times that button is push.
-pAgain = 0;
-pAgain2 = 0;
-pAgain3 = 0;
-pAgain4 = 0;
+   int pAgain = 0;
+   int pAgain2 = 0;
+   int pAgain3 = 0;
+   int pAgain4 = 0;
+
 
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
-  static int  blink_count = 0;
+  static int  blink_count = 0;         /* counter of blink timeson the MSP-430 */
 
   blink_count++;
-  if ((blink_count % 50) == 0 && pAgain == 1) {
+  if ((blink_count % 250) == 0 && pAgain == 1) {
     led_update();
     sound();
   }
-  
+  // state machine using the red LED.
+  else if ((blink_count % 500) == 0 && pAgain2 == 2){
+    led_update();
+     dim25r();
+     dim50r();
+     dim75r();
+     dim100r();
+  }
+  // state machine using gren LED.
+  else if ((blink_count % 750) == 0 && pAgain3 == 3){
+    led_update();
+     dim25g();
+     dim50g();
+     dim75g();
+     dim100g();
+  }
+  // state machine combination of green and red LED.
+  else if ((blink_count % 1000) == 0 && pAgain == 4){
+    led_update();
+     dim25gr();
+     dim50gr();
+     dim75gr();
+     dim100gr();
+  }
 }
