bea57f5680a519f25a4a49641caf23a725b767d5
student <zgomez4@miners.utep.edu>
2021-11-28 09:18:42 -0700

    added the three call for the 3 switch-statements on stateMachines.c

--- project/game/stateMachines.h
+++ project/game/stateMachines.h
@@ -1,27 +1,25 @@
 #ifndef stateMachine_included
 #define stateMachine_included
 
-
-
-// Red LED.
+ // Red LED.
  void dim25r();
  void dim50r();
  void dim75r();
- void dim100r();
+ void dimR(); // switch statement of red light.
 
  // Green LED.
  void dim25g();
  void dim50g();
  void dim75g();
- void dim100g();
-
+ void dimG();// switch statement of green light.
  // Combination of Green and Red LED.
  void dim25gr();
  void dim50gr();
  void dim75gr();
- void dim100gr();
+ void dimGR(); // switch statement of green and red light.
+
 
  // Secuence of sounds.
- void sound();
+void sound(); 
 
 #endif // included
