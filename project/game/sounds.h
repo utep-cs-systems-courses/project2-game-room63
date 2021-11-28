831afd1d48701ef9391631246f1bbbe60e60486e
student <zgomez4@miners.utep.edu>
2021-11-28 09:09:16 -0700

    fixing wrong sounds

--- project/game/sounds.h
+++ project/game/sounds.h
@@ -5,13 +5,13 @@
  /* notes that would make sound in the
     state machine */
 
-#define Dsharp 5142
+#define Dsharp 6430
 #define F 2290
-#define Fsharp 4324
-#define Gsharp 3853
-#define A 7272
-#define B 3239
-#define Cn 3057
-#define D 5448
+#define Fsharp 5420
+#define Gsharp 4819
+#define A 4545
+#define B 4056
+#define Cn 7662
+#define D 6825
 
 #endif
