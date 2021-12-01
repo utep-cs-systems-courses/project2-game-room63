cddd6a4498ed57659768a70af8f48a575a275a13
student <zgomez4@miners.utep.edu>
2021-11-30 18:53:00 -0700

    change all if-statements to switch cases

--- project/game/stateMachines.c
+++ project/game/stateMachines.c
@@ -15,44 +15,85 @@ static int stateG = 0;
 
   // state machine for the secuence of the red light.
   void dim25r() {
-  if (stateR== 0 || stateR== 1 || stateR== 2) {  // state 0, 1 and 2 turn off the red light 
-    Redlight(0);                         // red light off.
-    stateR++;
-    
-  }
-  else if (stateR== 3) {                 // state 3 turn on light.
-    Redlight(1);                         // red light on .
-    stateR = 0;
+    /* Switch statment to dimming the red LED 25%
+     stateR stands for state Red*/
+
+    switch(stateR){
+
+    case 1:
+      Redlight(0); // red LED off.
+      stateR++; 
+    case 2:
+      stateR++;
+    case 3:
+      stateR++;
+      break;     // red LED is off until the break.
+    case 4:
+      Redlight(1); // red LED on.
+      stateR = 0;       
+      break;
+    default:
+      break;
     }
   }
 
 
   void dim50r() {
-  if (stateR== 0 || stateR== 1) {        // state 0 and 1 turn off red light.
-    Redlight(0);
-    stateR++;
-  }
-  else if (stateR== 2 || stateR== 3) {  // state 2 and 3 turn on red light.
-    Redlight(1);
-    stateR = 0;
+    /* Switch statment to dimming the red LED 50%
+     stateR stands for state Red*/
+
+    switch(stateR){
+
+    case 1:
+      Redlight(1); //red LED on.
+      stateR++;
+      break;
+    case 2:
+      Redlight(0); // red LED off.
+      stateR++;
+      break;
+    case 3:
+      stateR++;
+      break;
+    case 4:
+      Redlight(1); // red LED on.
+      stateR = 0;
+      break;
+    default:
+      break;
     }
   }
 
   void dim75r(){
-    if (stateR== 0) {                  // only state 0 turn off led red.
-    Redlight(0);
-    stateR++;
-  }
-  else if (stateR== 1 || stateR== 2 || stateR== 3) {                                                 Redlight(1);
-  stateR = 0;
-   
-  }
- }
+    /* Switch statment to dimming the red LED 75%
+     stateR stands for state Red*/
+    
+    switch(stateR){
 
+    case 1:
+      Redlight(0);  // red LED is off.
+      stateR++;
+      break;
+    case 2:
+      stateR++;
+      break;
+    case 3:
+      stateR++;
+      break;
+    case 4:
+      Redlight(1); // red LED is on.
+      stateR = 0;
+      break;
+    default:
+      break;
+    }
+ }
+/*
 // Switch statement to call the secuence of the red light.
 void dimR (){
-  static int dimRS = 0;
 
+  static int dimRS = 0;
+  
   switch (dimRS){
   case 1:
     dim25r();
@@ -71,41 +112,86 @@ void dimR (){
   default:
     break;
   }
-}
+  
  
+ 
+}
+*/
 
   // state machine for the secuence of the green light.
   void dim25g() {
-  if (stateG == 0 || stateG == 1 || stateG == 2) {  // state 0, 1 and 2 turn off the green light 
-    Greenlight(0);                   // green light off.
-    stateG++;
+    /* Switch statment to dimming the green LED 25%
+     stateG stands for state Green*/
     
-  }
-  else if (stateG == 3) {           // state 3 turn on green light.
-    Greenlight(1);                 // green light on .
-    stateG = 0;
+    switch(stateG){
+
+    case 1:
+      Greenlight(0);  // green led off.
+      stateG++;
+    case 2:
+      stateG++;
+    case 3:
+      stateG++;
+      break;
+    case 4:
+      Greenlight(1);  // green led on.
+      stateG = 0;
+      break;
+    default:
+      break;
     }
+    
   }
 
 
   void dim50g() {
-  if (stateG == 0 || stateG == 1) {        // state 0 and 1 turn off green light.
-    Greenlight(0);
-    stateG++;
-  }
-  else if (stateG == 2 || stateG == 3) {  // state 2 and 3 turn on green light.
-    Greenlight(1);
-    stateG = 0;
+     /* Switch statment to dimming the green LED 50%
+     stateG stands for state Green*/
+
+    switch(stateG){
+
+    case 1:
+      Greenlight(1);  // green led on.
+      stateG++;
+      break;
+    case 2:
+      Greenlight(0); // green led off.
+      stateG++;
+      break;
+    case 3:
+      stateG++;
+      break;
+    case 4:
+      Greenlight(1); // green led on.
+      stateG = 0;
+      break;
+    default:
+      break;
     }
   }
 
   void dim75g() {
-  if (stateG == 0) {                     // only state 0 turn off led green.
-    Greenlight(0);
-    stateG++;
-  }
-  else if (stateG == 1 || stateG == 2 || stateG == 3) {                                                Greenlight(1);
-    stateG = 0;
+     /* Switch statment to dimming the green LED 75%
+     stateG stands for state Green*/
+
+    switch(stateG){
+
+    case 1:
+      Greenlight(0); // green led off.
+      stateG++;
+      break;
+    case 2:
+      stateG++;
+      break;
+    case 3:
+      stateG++;
+      break;
+    case 4:
+      Greenlight(1); // red led on.
+      stateG = 0;
+      break;
+    default:
+      break;
     }
   }
 
@@ -119,6 +205,7 @@ void dimG (){
     dimGS++;
     break;
 
+    
   case 2:
     dim50g();
     dimGS++;
@@ -126,7 +213,7 @@ void dimG (){
 
   case 3:
     dim75g();
-    dimGS++;
+    dimGS = 0;
     break;
   default:
     break;
@@ -186,7 +273,7 @@ void dimGR (){
 
   case 3:
     dim75gr();
-    dimGRS++;
+    dimGRS = 0;
     break;
   default:
     break;
@@ -212,5 +299,46 @@ void dimGR (){
   }
   else{
     Csound = 0; // Set current sound to 0.
+  }
+
+  // delete if not work.
+  void red(){
+    // state machine for the secuence of the red light.
+  void dim25r() {
+    if (stateR>= 0  || stateR == 1 || stateR == 2) {  // state 0, 1 and 2 turn off the red light 
+     Redlight(0);                         // red light off.
+     stateR++;
+  }
+  else if (stateR== 3) {                 // state 3 turn on light.
+    Redlight(1);                         // red light on .
+    stateR = 0;
+    }
+  
+  }
+
+
+  void dim50r() {
+    if (stateR== 0 || stateR== 1) {        // state 0 and 1 turn off red light.
+     Redlight(0);
+    stateR++;
+  }
+    else if (stateR== 2 || stateR== 3) {  // state 2 and 3 turn on red light.
+     Redlight(1);
+    stateR = 0;
+    }
+  }
+
+  void dim75r(){
+    if (stateR== 0) {                  // only state 0 turn off led red.
+    Redlight(0);
+    stateR++;
+  }
+  else if (stateR == 1 || stateR == 2 || stateR == 3) {
+  Redlight(1);
+  stateR = 0;
+   
+  }
+  }
+  
   }
 }
