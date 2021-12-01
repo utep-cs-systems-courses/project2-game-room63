1ae0ec5c5a6e08f9c46c785c864b60af83e642d6
student <zgomez4@miners.utep.edu>
2021-11-30 19:02:29 -0700

    the assembly part

---
+++ project/game/redSequenceS.s
@@ -0,0 +1,31 @@
+	.arch msp430g2553
+	.p2align 1, 0
+
+	.data
+redState:	.word 0
+	.text
+	.extern dim25r 		;dimining 25% on red LED.
+	.extern dim50r 		;dimining 50% on red LED.
+	.extern dim75r		;dimining 75% on red LED.
+	.global redSequence
+redSequence:
+	cmp #1, &redState
+	jl case_1 		;case 1 from redSecuence.
+	cmp #2, &redState
+	jl case_2 :		;case 2 from redSecuence.
+	cmp #3, &redState
+	jl case_3 		;case 3 from red secuence.
+case_1:
+	call #dim25r
+	add #1, &redState
+	jmp out
+case_2:
+	call #dim50r
+	add #1, &redState
+	jmp out
+case_3:
+	call #dim575r
+	mov #0, &redState
+	jmp out
+out:
+	pop r0
