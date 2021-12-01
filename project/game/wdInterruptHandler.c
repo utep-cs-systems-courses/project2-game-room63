f5c949de8b2f9d53658021d8486295c2daf16d1b
student <zgomez4@miners.utep.edu>
2021-12-01 14:11:07 -0700

    some modifications

--- project/game/wdInterruptHandler.c
+++ project/game/wdInterruptHandler.c
@@ -16,84 +16,46 @@ void __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static int  blink_countR = 0;
   blink_count++;
   blink_countR++;
-  
-  // blink_count % 63) == 0 && pAgain == 1
+  redSequence(0); // The switch statemment that I try to run on button 4.
+  led_on(0); // to turn in on LED green and red.    
+  //changeC(0); 
   if ((blink_count % 63) == 0 && pAgain == 1) {
     buzzer_set_period(0); // turn of completly the sound of sound();
     led_update();
-    sound();  // secuence of sounds.x
+    sound();  // secuence of sounds is on stateMachines.c
+    led_on(1);
     blink_count = 0;
     
-  }
-  /*
-  else if ((blink_countR < 125) == 0 && pAgain2 == 1){
-     if(blink_countR < 125){
-    
-       blink_countR = 0;
-    }
-     
-    else if (blink_countR < 500){
-      dim50r();
-      blink_countR = 0;
-    }
-    else if (blink_countR < 750){
-      dim75r();
-      blink_countR++;
-      }
+  }// end of if-statement.
+  
+   if ((blink_countR % 8 == 0) && pAgain2 == 1){
+     led_update();
+     // dim25r();
+     // led_on(1);
+     Greenlight(1); // for some reason it turn on the red LED.
+     Redlight(1); // // for some reason it turn on the green LED.
+     // changeC(1);
+     //redSequence();
+    blink_count = 0;
+    // sound();
       
-  }
-  */
+  }// end of if-statement.
+
+   
+  
   // state machine using the red LED.
-  if ((blink_countR % 63) == 0 && pAgain2 == 1){
-    // led_update();
-       
-  // if (blink_count < 63){
-      //dimR(); // switch statment for red light.
-  //   dim25r();
-       
-  //}
-    
-  //else if ( blink_count < 125){
-  //  dim50r();
-    
-  //}
-  //else if (blink_count < 188){
+  if ((blink_countR % 63) == 0 && pAgain3 == 1){
+    buzzer_set_period(0); // turn of completly the sound of sound();
     led_update();
-    redSequence();
+    sound2();  // secuence of sounds.
+    blink_count = 0;
+  }// end of if-statement.
+
+  if ((blink_countR % 63 == 0) == 0 && pAgain4 == 1){
+    led_update();
+    redSequence(); // switch statement that hold all the switch corresponding to RED LED
+                   // redSequence.c
     blink_countR = 0;
-  }
-
+  }// end of if-statement.
   
-  // state machine using gren LED.
-  else if ((blink_count % 125) == 0 && pAgain3 == 1){
-    led_update();
-         
-    if (blink_count < 63){
-      //dimG(); // switch statment for green light.
-      //led_update();
-      dim25g();
-    }
-    else if ( blink_count < 125){
-      dim50g();
-    }
-    else if (blink_count < 188){
-      dim75g();
-    }
-  
-    //dimG(); // switch statment for green light.
-     blink_count = 0;
-  }
-
-  
-  /*  
-  // state machine combination of green and red LED.
-  else if ((blink_count % 250) == 0 && pAgain4 == 1){
-    led_update();
-    
-     dimGR(); // switch statment for green and red light.
-     blink_count = 0;
-  }
-  
-  */  
-
-}
+} // end of interrupt.
