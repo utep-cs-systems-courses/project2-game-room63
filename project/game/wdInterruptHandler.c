72d56ca23121be407f1778689f61a9a9f15ce287
student <zgomez4@miners.utep.edu>
2021-12-03 17:50:34 -0700

    added the new switch statement call and fixing some bugs

--- project/game/wdInterruptHandler.c
+++ project/game/wdInterruptHandler.c
@@ -9,53 +9,43 @@
      int pAgain2 = 0;
      int pAgain3 = 0;
      int pAgain4 = 0;
-
 void __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
 
   static int  blink_count = 0;         /* counter of blink timeson the MSP-430 */
-  static int  blink_countR = 0;
-  blink_count++;
-  blink_countR++;
-  redSequence(0); // The switch statemment that I try to run on button 4.
-  led_on(0); // to turn in on LED green and red.    
-  //changeC(0); 
+  static int  blink_countR = 1;
+  static int   blink_countG = 1;
+  blink_countR++; // counter for the redSequence() function.
+  blink_count++; // counter for the sound() adn sound2() functions.
+    
   if ((blink_count % 63) == 0 && pAgain == 1) {
     buzzer_set_period(0); // turn of completly the sound of sound();
     led_update();
     sound();  // secuence of sounds is on stateMachines.c
-    led_on(1);
     blink_count = 0;
     
   }// end of if-statement.
-  
-   if ((blink_countR % 8 == 0) && pAgain2 == 1){
-     led_update();
-     // dim25r();
-     // led_on(1);
-     Greenlight(1); // for some reason it turn on the red LED.
-     Redlight(1); // // for some reason it turn on the green LED.
-     // changeC(1);
-     //redSequence();
-    blink_count = 0;
-    // sound();
+  // need it to press four times to see the different light dimming.
+  else if ((blink_countG % 125) && pAgain2 == 1){
+    led_update();
+    greenSequence();
+    blink_countG = 1;
+   
       
   }// end of if-statement.
 
-   
-  
+    
   // state machine using the red LED.
-  if ((blink_countR % 63) == 0 && pAgain3 == 1){
+   else if ((blink_count % 63) == 0 && pAgain3 == 1){
     buzzer_set_period(0); // turn of completly the sound of sound();
     led_update();
     sound2();  // secuence of sounds.
     blink_count = 0;
   }// end of if-statement.
 
-  if ((blink_countR % 63 == 0) == 0 && pAgain4 == 1){
+   else if ((blink_count % 125) == 0 && pAgain4 == 1){
     led_update();
-    redSequence(); // switch statement that hold all the switch corresponding to RED LED
-                   // redSequence.c
-    blink_countR = 0;
+    redSequence();
+    blink_countR = 1;
   }// end of if-statement.
-  
+
 } // end of interrupt.
